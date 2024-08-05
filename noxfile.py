2607c5d766a7fa914d4de8f2fbf396af97345d95
Ferran Brosa Planella <Ferran.Brosa-Planella@warwick.ac.uk>
2024-08-05 11:26:34 +0100

    fix coverage

--- noxfile.py
+++ noxfile.py
@@ -56,9 +56,13 @@ def run_user_tests(session):
 @nox.session(name="coverage")
 def run_coverage(session):
     """Run the coverage tests and generate an XML report."""
-    session.posargs.append("--cov=src/electrode_coating")
-    session.posargs.append("--cov-report=xml")
-    run_user_tests(session)
+    session.install("setuptools", silent=False)
+    session.install("coverage", silent=False)
+    session.run("pytest", "--cov=src/electrode_coating", "--cov-report=xml", "tests/user_tests")
+    # session.posargs.append("--cov=src/electrode_coating")
+    # session.posargs.append("--cov-report=xml")
+    # run_user_tests(session)
+
 
 
 @nox.session(name="dev")
